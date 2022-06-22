aa4285ce22b521698a13fdc9d987d73b30ef415f
Shubham <shubhos@ubuntu.ubuntu-domain>
2022-06-22 16:29:16 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      This is about compoment
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, ab. Expedita, itaque. Repellat facere id iure assumenda omnis eaque voluptatum porro? Deleniti molestiae aliquid praesentium blanditiis culpa sit illo aperiam expedita itaque recusandae magnam dolore, fugiat suscipit voluptates harum eum.</p>
+    </div>
+  )
+}
+
+export default About
